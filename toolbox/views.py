35d36d79241e9b5d1704bf7d6d4656d0e8d7a2ee
ChefPachimari <bboyteddybear@gmail.com>
2024-12-16 15:45:33 -0500

    add check for the triplet and fix saving the last request and request counter

--- toolbox/views.py
+++ toolbox/views.py
@@ -16,29 +16,42 @@ from .models import Numbers
 @api_view(['GET'])
 def difference(request):
     # Get input and return errors early
-    n = request.query_params.get('number', None)
-    if n is None:
+    num = int(request.query_params.get('number', None))
+    if num is None:
         return Response({"error": "input 'number' not found"}, status=status.HTTP_400_BAD_REQUEST)
     # check to see if the number exists
     try:
-        numbers = Numbers.objects.filter(number__lte=n)
+        numbers = Numbers.objects.filter(number__lte=num)
     except Numbers.DoesNotExist:
         return Response({"error": "number not found"}, status=status.HTTP_404_NOT_FOUND)
 
+    # use F expressions to perform arithmetic, then aggregates
     sum_of_squares = numbers.aggregate(sum_of_squares=Sum(F('number') ** 2))['sum_of_squares']
     square_of_sum = numbers.aggregate(square_of_sum=Sum('number'))['square_of_sum'] ** 2
 
+    # there's only possible triplet and that is 3,4,5 which abc = 60
+    # so we look for 60 otherwise we return an empty list
+    # always returning a list as maintain consistent data types in json response
+    has_triplet = []
+    if num == 60:
+        print("found 60")
+        has_triplet = [3,4,5]
     data = {
             "datetime": datetime.now(),
             "value": abs(square_of_sum - sum_of_squares),
-            "number": n,
+            "number": num,
             "occurences": numbers.last().request_ct,
-            "last_datetime": numbers.last().request_last_timestamp  # since n is based on the last object / up to the last object
+            "last_datetime": numbers.last().request_last_timestamp,  # since n is based on the last object / up to the last object
+            "has_triplet": has_triplet
         }
     
     # is this worth putting in a post save signal? -thn 15:15
     # no because post signal would trigger with admin saves, not just on requests -thn 15:18
-    numbers.last().request_last_timestamp = datetime.now()
-    numbers.last().request_ct += 1
+
+    update_number = numbers.last()
+    update_number.request_last_timestamp = datetime.now()
+    update_number.request_ct += 1
+    update_number.save()
+
 
     return Response(data, status=status.HTTP_200_OK)
