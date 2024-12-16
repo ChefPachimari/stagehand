26182386714563ad4699080e53af992752daa96c
ChefPachimari <bboyteddybear@gmail.com>
2024-12-16 15:19:30 -0500

    rename request_last_request to request_last_timestamp due to goofy naming convention

--- toolbox/models.py
+++ toolbox/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Numbers(models.Model):
     number = models.IntegerField()
     request_ct = models.IntegerField()
+    request_last_timestamp = models.DateTimeField(null=True, blank=True)
