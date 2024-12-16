c9fbcab7ac337744e3e0a6dbb643b76422ba4106
ChefPachimari <bboyteddybear@gmail.com>
2024-12-16 11:12:23 -0500

    add redis to the initial dev to ensure it exists, simplify the script with the idea this is for initializing development/running the project

--- initial_dev.sh
+++ initial_dev.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # Create a virtual environment named 'venv'
-python3 -m venv venv
-if [ -d "venv" ]; then
-    echo "venv already exists. run 'source venv/bin/activate'"
+
+# Prompt the user to confirm Redis installation
+read -p "Redis will be installed on the system. Do you want to continue? (y/n): " confirm
+if [[ $confirm != [yY] ]]; then
+    echo "Redis installation aborted."
     exit 1
 fi
+
+# Install Redis
+sudo apt-get update
+sudo apt-get install -y redis-server
+
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
 
 # Initial Install the dependencies from requirements.txt
 pip install -r requirements.txt
